d19e867490c32868db6e9ce195b0a349a63b5c37
tahsin005 <tahsin.ferdous3546@gmail.com>
2026-04-05 16:12:46 +0600

    feat: add weather dashboard with Card and DailyForecast components, integrate OpenWeather API via getWeather, validate data with Zod schema, update deps, and adjust gitignore

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,22 @@
+import { useQuery } from "@tanstack/react-query"
+import { getWeather } from "./api"
+import Card from "./components/cards/Card"
+import DailyForecast from "./components/cards/DailyForecast"
+
 function App() {
+  const { data } = useQuery({
+    queryKey: ["weather"],
+    queryFn: () => getWeather({ lat: 23.8103, lon: 90.4125 }),
+  })
   return (
-    <div className="size-32 bg-red-500">
+    <div className="flex flex-col gap-8">
+      <Card title="Current Weather">
+        {JSON.stringify(data?.current)}
+      </Card>
+      <Card title="Hourly Forecast (48 Hours)">
+        {JSON.stringify(data?.hourly)}
+      </Card>
+      <DailyForecast />
     </div>
   )
 }
