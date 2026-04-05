01c41cefaeb8027b31657b5f555ac300631809ea
tahsin005 <tahsin.ferdous3546@gmail.com>
2026-04-05 18:20:27 +0600

    feat: add CurrentWeather component, integrate WeatherIcon for displaying current weather, and update Card component to accept className prop

--- src/components/WeatherIcon.tsx
+++ src/components/WeatherIcon.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import clsx from "clsx"
 
 type Props = {
   icon: string
+  className?: string
 }
 
-export default function WeatherIcon({ icon }: Props) {
+export default function WeatherIcon({ icon, className }: Props) {
     return (
-        <img className='size-8' src={`https://openweathermap.org/payload/api/media/file/${icon}.png`} alt='weather icon'/>
+        <img className={clsx('size-8', className)} src={`https://openweathermap.org/payload/api/media/file/${icon}.png`} alt='weather icon'/>
     )
 }
--- src/components/cards/Card.tsx
+++ src/components/cards/Card.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
+import clsx from "clsx"
 
 type Props = {
     children: React.ReactNode,
     title: string,
+    className?: string,
     childrenClassName?: string,
 }
 
-export default function Card({ children, title, childrenClassName }: Props) {
+export default function Card({ children, title, className, childrenClassName }: Props) {
     return (
-        <div className='p-4 rounded-xl bg-zinc-900 shadow-md flex flex-col gap-4'>
+        <div className={clsx(
+            'p-4 rounded-xl bg-zinc-900 shadow-md flex flex-col gap-4',
+            className
+        )}>
             <h2 className='text-2xl font-semibold'>{title}</h2>
             <div className={childrenClassName}>
                 {children}
